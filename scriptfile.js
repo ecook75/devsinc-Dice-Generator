c884155ed2acd96277f8b0615aa1b5d9548de837
ecook75 <eppeycook2@gmail.com>
2025-07-15 18:19:07 +1200

    script commit

---
+++ scriptfile.js
@@ -0,0 +1,21 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max) +1;
+}
+
+console.log(getRandomInt(6));
+// Expected output: 0, 1 or 2
+
+console.log(4<6)
+console.log(4>6)
+console.log(getRandomInt(6));
+console.log(getRandomInt(6));
+console.log(getRandomInt(6));
+console.log(getRandomInt(6));
+
+if (getRandomInt(6) > 6 ) { throw new Error('Test failed');}
+console.log('All tests passed.');
+if (getRandomInt(6) < 1 ) { throw new Error('Test failed');}
+if (getRandomInt(10) > 10) { throw new Error('Test failed');}
+console.log('All tests passed.');
+if (getRandomInt(10) < 1 ) { throw new Error('Test failed');}
+
